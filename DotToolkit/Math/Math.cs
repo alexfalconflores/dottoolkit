f96e849523e22749ca10368ff7f18e263b81cf48
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-09-10 15:10:32 -0500

    [ADD] Even and odd numbers

--- DotToolkit/Math/Math.cs
+++ DotToolkit/Math/Math.cs
@@ -1,7 +1,15 @@
-﻿namespace DotToolkit.Math;
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DotToolkit.Math;
 
 public static class Math
 {
+    /// <summary>
+    /// Get
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
     public static ulong Fibonacci(this int n)
     {
         if (n == 0) return 0;
@@ -14,5 +22,38 @@ public static class Math
             b = result;
         }
         return result;
+    }
+    /// <summary>
+    /// Check if a number is even
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public static bool IsEven(this int number) => number % 2 == 0;
+    /// <summary>
+    /// Return a sequence of even numbers up to the given number.
+    /// <para>Output: { 0, 2, 4, 6, 8, 10}</para>
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public static IEnumerable<int> Evens(this int number)
+    {
+        for (int i = 0; i <= number; i += 2)
+            yield return i;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public static bool IsOdd(this int number) => number % 2 != 0;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    public static IEnumerable<int> Odds(this int number)
+    {
+        for (int i = 1; i <= number; i += 2)
+            yield return i;
     }
 }
