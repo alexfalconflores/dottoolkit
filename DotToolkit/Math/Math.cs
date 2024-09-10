29a03011c4af4ee0784f7ee52a852b6a41a0cf40
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-09-10 01:47:57 -0500

    [ADD] Fibonacci

--- DotToolkit/Math/Math.cs
+++ DotToolkit/Math/Math.cs
@@ -1,6 +1,18 @@
 ﻿namespace DotToolkit.Math;
 
-public partial class Math
+public static class Math
 {
-
+    public static ulong Fibonacci(this int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        ulong a = 0, b = 1, result = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
 }
