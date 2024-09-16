e8324d046cd2f9ca7acc4c92904d253d0333132b
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-09-16 03:24:18 -0500

    [ADD] IsNaN

---
+++ DotToolkit/Extensions/NumberExt.cs
@@ -0,0 +1,27 @@
+﻿namespace DotToolkit.Extensions;
+
+public static class NumberExt
+{
+    /// <summary>
+    /// Check if the number is NaN.
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    /// <example><code>
+    /// double value = doble.NaN;
+    /// var result = value.IsNaN()
+    /// // -> true
+    /// </code></example>
+    public static bool IsNaN(this double number) => double.IsNaN(number);
+    /// <summary>
+    /// Check if the number is NaN.
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    /// <example><code>
+    /// float value = float.NaN;
+    /// var result = value.IsNaN()
+    /// // -> true
+    /// </code></example>
+    public static bool IsNaN(this float number) => float.IsNaN(number);
+}
